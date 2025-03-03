6253cfb11b27bdfa11faa4cedaf86b6ef74590c5
Danielen Cestari <danielencestari@Lucianos-MacBook-Pro.local>
2025-03-03 13:10:04 -0300

    aula 2 bloco 6

--- 6/1/go.mod
+++ 6/1/go.mod
@@ -2,4 +2,9 @@ module github.com/danielencestari/aulas_pos/6/1
 
 go 1.24.0
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.9.0
+	github.com/google/uuid v1.6.0
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- 6/1/main.go
+++ 6/1/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
 
@@ -18,5 +21,31 @@ func NewProduct(name string, price float64) *Product {
 	}
 }
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/aulas_pos")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
+	// inserir um novo produto - depois que fez a proteção contra SQL Injection
+	product := NewProduct("Produto 2", 450.00)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	// proteger contra SQL Injection
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
 }
