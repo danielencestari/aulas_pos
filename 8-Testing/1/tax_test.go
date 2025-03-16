629ded0e76e9b039374216c43d8a4cf2ab74f2b4
Danielen Cestari <danielencestari@Lucianos-MacBook-Pro.local>
2025-03-16 19:34:54 -0300

    testes

--- 8-Testing/1/tax_test.go
+++ 8-Testing/1/tax_test.go
@@ -34,3 +34,51 @@ func TestCalculateTaxBatch(t *testing.T) {
 	}
 
 }
+
+// para ver a cobertura dos testes, executar:
+// go test -coverprofile=coverage.out
+// para ver onde não foi coberto, tem que executar:
+// go tool cover -html=coverage.out
+// ai ele abre o browser com o relatório de cobertura, e o que está em vermelho não foi coberto
+
+// teste de benchmark usa o b do tipo testing.B
+// para rodar o benchmark, executar:
+// go test -bench=.
+// na aula o resultado do prof foi diferente, pois mostrou mais detalhes da operacao
+// para ver mais detalhes, executar
+// go test -bench=. -benchmem
+func BenchmarkCalculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax(500)
+	}
+}
+
+// rodando: go test -bench=. -run=ˆ#
+// da pra ver a diferença do tempo de execução entre os dois testes
+// o temporizador incluido na func CalculateTax2 faz com que ela seja mais lenta
+// se rodar go test -bench=. -run=ˆ# -count=10, ele roda 10 vezes
+// se rodar go test -bench=. -run=ˆ# -count=10 -benchtime=10s, ele roda 10 vezes e cada vez dura 10s
+func BenchmarkCalculate2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax2(500)
+	}
+}
+
+// Fuzzing - testes de mutação
+// o fuzz é tipo uma rede neural que tenta encontrar falhas no código
+// vc tem que "treinar" um pouco o fuzz, para ele entender o que é um erro
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1501.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Reveived %f but expected 0", result)
+		}
+		if amount > 20000 && result != 20 {
+			t.Errorf("Reveived %f but expected 20", result)
+		}
+	})
+}
