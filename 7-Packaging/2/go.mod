6b227aa8e1f9b9d0d8223687b254b2f6f6d91bc4
Danielen Cestari <danielencestari@Lucianos-MacBook-Pro.local>
2025-03-16 16:02:36 -0300

    modulo 7.2

--- 7-Packaging/2/go.mod
+++ 7-Packaging/2/go.mod
@@ -2,5 +2,9 @@ module github.com/danielencestari/aulas_pos/7-Packagin/2
 
 go 1.24.0
 
-// para iniciar esse mod o comando foi : go mod init github.com/danielencestari/aulas_pos/7-Packagin/2  
+// para iniciar esse mod o comando foi : go mod init github.com/danielencestari/aulas_pos/7-Packagin/2
 
+require github.com/google/uuid v1.6.0
+
+// ai agora esse pacote de google uuid us normal, pq eu dei o go mod tidy, então ele instalou o pacote
+// go mod tidy garante que as dependencias fiquem ok
--- 7-Packaging/2/main.go
+++ 7-Packaging/2/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/googlr/uuid"
+import "github.com/google/uuid"
 
 func main() {
 	// trabalhar como se quisesse imprimir um uuid
-	// digitar o import "github.com/googlr/uuid" mas ainda sem dar o go mod tidy, e ele não funcionará
+	// digitar o import "github.com/google/uuid" mas ainda sem dar o go mod tidy, e ele não funcionará
 	println(uuid.New().String())
 }
+
+// ao executar  go run main.go o resultado é
+// no required module provides package github.com/googlr/uuid; to add it:
+// go get github.com/googlr/uuid  ou no caso, dar um go mod tidy pra instalar o pacote
