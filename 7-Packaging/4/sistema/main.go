d98f375ea08d81ad55f2b9aacce8b4265e2bfb80
Danielen Cestari <danielencestari@Lucianos-MacBook-Pro.local>
2025-03-16 17:39:32 -0300

    fim mod 07

--- 7-Packaging/4/sistema/main.go
+++ 7-Packaging/4/sistema/main.go
@@ -1,14 +1,12 @@
 package main
 
-import "github.com/danielencestari/aulas_pos/7-Packaging/3/math"
+import (
+	"github.com/danielencestari/aulas_pos/7-Packaging/3/math"
+)
 
 func main() {
-	// agora quero usar o pacote math
-	// para isso, preciso importar o pacote
 
 	m := math.NewMath(1, 2)
 	println(m.Add())
-}
 
-// formas de publicar o pacote math pra usa-lo aqui
-// comando go mod edit -replace github.com/danielencestari/aulas_pos/7-Packaging/3/math=../math
+}
